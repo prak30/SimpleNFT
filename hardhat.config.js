9ed4e08821be377c1b48b3502306b4ed8cb61041
pranav <katkarpranav4@gmail.com>
2022-11-22 00:47:36 +0530

    fixed typos and changed to mumbai polygon network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,4 @@
-require(“@nomicfoundation/hardhat-toolbox”);
+require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
 
 const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
@@ -7,7 +7,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    goerli: {
+    mumbai: {
       url: QUICKNODE_HTTP_URL,
       accounts: [PRIVATE_KEY],
     },
